93a3fad3ac6972e05c3b46fc59a6f47085327fb0
NicolasDeza <nicolasdeza@hotmail.be>
2025-08-24 16:59:43 +0200

    Modif token xsrf

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,11 +3,5 @@ window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-const token = document.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common["X-CSRF-TOKEN"] = token.content;
-} else {
-    console.error(
-        'CSRF token not found: check <meta name="csrf-token"> in app.blade.php'
-    );
-}
+window.axios.defaults.xsrfCookieName = "XSRF-TOKEN";
+window.axios.defaults.xsrfHeaderName = "X-XSRF-TOKEN";
